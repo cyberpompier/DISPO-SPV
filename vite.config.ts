1c81c32de51a61f2cf4eb34a20972d645e14e920
[dyad] <sebastien.dupressoir@icloud.com>
2026-04-05 22:06:30 +0200

    [dyad] add Dyad component tagger

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+  plugins: [dyadComponentTagger(), 
     react(),
     VitePWA({
       registerType: 'autoUpdate',
